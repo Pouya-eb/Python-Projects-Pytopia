3cff6bb932050745a1266d4e0e137cd046f1fa2a
Pouya-eb <pouyaebrahimi1379@gmail.com>
2024-07-07 19:12:04 +0330

    Testing main.py

--- 07-Contact-Book/src/main.py
+++ 07-Contact-Book/src/main.py
@@ -57,3 +57,50 @@ class ContactBook:
         
         print('Contact not found')
 
+
+if __name__ == '__main__':
+    contact_book = ContactBook()
+
+    while True:
+        print()
+        print('\t Contact Book Application')
+        print('1. Add Contact')
+        print('2. Edit Contact')
+        print('3. View Contact')
+        print('4. Delete Contact')
+        print('5. Quit')
+        print()
+        user_choice = input('Please enter your choice: ')
+
+        if user_choice == '1':
+            print()
+            name = input('Enter contact name: ')
+            phone = input('Enter contact phone number: ')
+            email = input('Enter contact email address: ')
+            contact_book.add_contact(name, phone, email)   
+        
+        elif user_choice == '2':
+            print()
+            name = input('Enter name of the contact to edit: ')
+            phone = input('Enter new contact phone number or press Enter to keep unchanged: ')
+            email = input('Enter new contact email or press Enter to keep unchanged: ')
+            contact_book.update_contact(name, phone or None, email or None)
+
+        elif user_choice == '3':
+            print()
+            print('List of contacst: ')
+            contact_book.view_contacts()  
+
+        elif user_choice == '4':
+            print()
+            name = input('Enter name of contact to delete: ')
+            contact_book.delete_contact(name)
+        
+        elif user_choice == '5':
+            print()
+            print('Thank you for using Contact Book Application. Goodbye!')
+            break
+
+        else:
+            print()
+            print('Invalid input! Please try again.')
